43be8cecee7c370a4d945fa5f0abf592c2d103a7
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-19 14:40:22 -0600

    bigger, better, uncut

--- main.js
+++ main.js
@@ -1,60 +1,41 @@
 var numberOfVictims = prompt("How many disaster victims are you entering?");
-
 var victimNameArray = [];
-
 var victimPhoneArray = [];
-
 var victimStreetArray = [];
+var numberOfVolunteers = prompt("How many disaster volunteers are you entering?");
+var volunteerNameArray = [];
+var volunteerPhoneArray = [];
+var volunteerStreetArray = [];
 
 for(var i = 0; i<numberOfVictims; i++) {
 	
 	// Victim Name
 	var victimName = prompt("What's the victim's name?");
-	console.log("victimName:", victimName);
 	victimNameArray.push(victimName);
-	console.log("victimNameArray:", victimNameArray);
 	
 	// Victim Phone
 	var victimPhone = prompt("What's the victim's phone number?");
-	console.log("victimPhone:", victimPhone);
 	victimPhoneArray.push(victimPhone);
-	console.log("victimPhoneArray", victimPhoneArray);
 	
 	// Victim Street
 	var victimStreet = prompt("What street does the victim live on?");
-	console.log("victimStreet:", victimStreet);
 	victimStreetArray.push(victimStreet);
-	console.log("victimStreetArray:", victimStreetArray);
 
 };
-
-var numberOfVolunteers = prompt("How many disaster volunteers are you entering?");
-
-var volunteerNameArray = [];
-
-var volunteerPhoneArray = [];
-
-var volunteerStreetArray = [];
 
 for(var i = 0; i<numberOfVolunteers; i++) {
 	
 	// Volunteer Name
 	var volunteerName = prompt("What's the volunteer's name?");
-	console.log("volunteerName:", volunteerName);
 	volunteerNameArray.push(volunteerName);
-	console.log("volunteerNameArray:", volunteerNameArray);
 	
 	// Volunteer Phone
 	var volunteerPhone = prompt("What's the volunteer's phone number?");
-	console.log("volunteerPhone:", volunteerPhone);
 	volunteerPhoneArray.push(volunteerPhone);
-	console.log("volunteerPhoneArray", volunteerPhoneArray);
 	
 	// Volunteer Street
 	var volunteerStreet = prompt("What street does the volunteer live on?");
-	console.log("volunteerStreet:", volunteerStreet);
 	volunteerStreetArray.push(volunteerStreet);
-	console.log("volunteerStreetArray:", volunteerStreetArray);
 
 };
 
